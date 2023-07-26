e13b0f5ac9fa653131ff1f3e7f9395d0581d2a37
danya2271 <danya2271@yandex.ru>
2023-07-26 23:35:32 +0500

    Ld is lld

--- clang.sh
+++ clang.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-make CC=$(pwd)/clang/bin/clang-15 \
+make CC=$(pwd)/clang/bin/clang-15 LD=$(pwd)/clang/bin/ld.lld \
 CROSS_COMPILE_ARM32=$(pwd)/gcc-linaro-13.0.0-2022.10-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf- \
 CROSS_COMPILE=$(pwd)/gcc-linaro-13.0.0-2022.10-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- \
 O=out ARCH=arm64 -j$(($(nproc)+1)) $@
