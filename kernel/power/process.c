36b8e47eb49b0b0da08c5cc9cfd6714f75ecdd23
danya2271 <danya2271@yandex.ru>
2023-08-15 18:37:51 +0500

    Few fixes

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -196,7 +196,7 @@ void thaw_fingerprintd(void)
 
 	read_lock(&tasklist_lock);
 	for_each_process(p) {
-	        if (!strstr(p->comm, "erprint")) {
+	        if (!strstr(p->comm, "android.hardware.biometrics.fingerprint@2.1-service")) {
 			__thaw_task(p);
 			break;
 		}
