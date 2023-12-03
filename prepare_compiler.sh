35506c558dceded97220b4c0cfc6927e9b5a2743
danya2271 <danya2271@.yandex.ru>
2023-12-03 14:27:12 +0500

    Update compiler scripts

--- prepare_compiler.sh
+++ prepare_compiler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git clone https://gitlab.com/LeCmnGend/proton-clang.git clang --depth 1
-wget https://snapshots.linaro.org/gnu-toolchain/13.0-2022.10-1/aarch64-linux-gnu/gcc-linaro-13.0.0-2022.10-x86_64_aarch64-linux-gnu.tar.xz
-tar -xf gcc-linaro-13.0.0-2022.10-x86_64_aarch64-linux-gnu.tar.xz
+git clone https://github.com/CruelKernel/aarch64-cruel-elf --depth 1
+
