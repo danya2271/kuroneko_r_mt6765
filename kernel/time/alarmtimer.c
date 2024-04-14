0d3117a00423a3dfb5e38ae063577fde641b405e
danya2271 <danya2271@.yandex.ru>
2024-04-15 02:02:37 +0500

    Minimize alarmtimer wakeup time

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -294,10 +294,8 @@ static int alarmtimer_suspend(struct device *dev)
 	if (min == 0)
 		return 0;
 
-	if (ktime_to_ns(min) < 2 * NSEC_PER_SEC) {
-		__pm_wakeup_event(ws, 2 * MSEC_PER_SEC);
-		return -EBUSY;
-	}
+	if (ktime_to_ns(min) < 2 * NSEC_PER_SEC)
+		__pm_wakeup_event(ws, ktime_to_ms(min) + 10);
 
 	trace_alarmtimer_suspend(expires, type);
 
