8e97727056ac6cfa428d2bf0cba664435e257668
danya2271 <dany2271@yandex.ru>
2024-06-18 22:51:20 +0500

    prepare_compiler.sh: Switch to latest clang 18

--- prepare_compiler.sh
+++ prepare_compiler.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir clang
 cd clang
-bash <(curl -s https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman) -S
+bash <(curl -s https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman) -S=10032024
 cd ..
 wget https://snapshots.linaro.org/gnu-toolchain/13.0-2022.10-1/aarch64-linux-gnu/gcc-linaro-13.0.0-2022.10-x86_64_aarch64-linux-gnu.tar.xz
 tar -xf gcc-linaro-13.0.0-2022.10-x86_64_aarch64-linux-gnu.tar.xz
